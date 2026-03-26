1023755695c4405f40fa5d66fbb351455615929e
Colton Crouch <colton.crouch96@gmail.com>
2026-03-26 12:42:37 -0400

    Create DbContext, register EF and OpenIddict in services

---
+++ src/See.Idp.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace See.Idp.Infrastructure;
+
+/// <summary>
+///     The Entity Framework Core database context to handle authentication and authorization data.
+/// </summary>
+/// <param name="options">The options to be used by a <see cref="DbContext"/>.</param>
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+}
--- src/See.Idp.Web/Program.cs
+++ src/See.Idp.Web/Program.cs
@@ -1,8 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using See.Idp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configure Entity Framework Core to use PostgreSQL.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    options.UseNpgsql(connectionString);
+
+    // Register the entity sets needed by OpenIddict.
+    // Note: use the generic overload if you need to replace the default OpenIddict entities.
+    options.UseOpenIddict();
+});
 
 var app = builder.Build();
 
@@ -14,28 +34,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
