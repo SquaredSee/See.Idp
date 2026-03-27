a3bbe97ad7ad1e6bfaeb74eb9545dbc0d9cbe27f
Colton Crouch <colton.crouch96@gmail.com>
2026-03-27 18:13:07 -0400

    Cleanup

--- src/See.Idp.Web/Pages/Error.cshtml.cs
+++ src/See.Idp.Web/Pages/Error.cshtml.cs
@@ -3,19 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace See.Idp.Web.Pages
+namespace See.Idp.Web.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    [IgnoreAntiforgeryToken]
-    public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
